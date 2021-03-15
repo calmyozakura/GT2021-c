16d606d7558a7378956f9a6ee92f6d3aa6ce7513
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-03-15 10:56:37 +0900

    ファイル分割用のテストステージ　消去アニメーションの修正

---
+++ iromawasi/Assets/Script/test/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    Text scoreText;
+
+    int score = 0;      //スコア
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScoreAdd(int[] bonusLevel,int check)
+    {
+        //スコア+100と各パネルのボーナス分スコア+50
+        score += 100 + (50 * (bonusLevel[(check / 3) + check] + bonusLevel[(check / 3) + check + 1]
+            + bonusLevel[(check / 3) + check + 5] + bonusLevel[(check / 3) + check + 4]));
+
+        scoreText.text = "" + score;
+    }
+}
---
+++ iromawasi/Assets/Script/test/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text timerText;
+    float timeCount = 60.0f;            //制限時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    public void TimerCount()
+    {
+        if (timeCount >= 0)
+        {
+            timeCount -= Time.deltaTime;    //制限時間のカウントダウン
+
+            timerText.text = timeCount.ToString("f1");  //時間の表示
+        }
+    }
+}
